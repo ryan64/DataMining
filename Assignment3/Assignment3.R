4f0d9b79e6eff88406969b6dd33bd83e7f2e9908
ryan.gonzalez64@gmail.com <ryan.gonzalez64@gmail.com>
2018-04-11 15:24:37 -0500

    finished svm

--- Assignment3/Assignment3.R
+++ Assignment3/Assignment3.R
@@ -24,22 +24,15 @@ folds = split(spamnorm, sample(1:10, nrow(spamnorm), replace=T))
 install.packages("e1071")
 library(e1071)
 
-svmfit = list()
-predictedOutcomes = list()
+error = list()
 for (i in 1:10){
   trainData <- folds[-i] #trained data is every fold except for the current ith iteration.
   trainData = do.call('rbind', trainData)
-  svmfit[i] <- svm(V58 ~ V1 + V2 + V3 + V4 + V5 + V6 + V7 + V8 + V9 + V10 + V11 + V12 + V13 + V14 + V15 + V16 + V17 + V18 + V19 + V20 + V21 + V22 + V23 + V24 + V25 + V26 + V27 + V28 + V29 + V30 + V31 + V32 + V33 + V34 + V35 + V36 + V37 + V38 + V39 + V40 + V41 + V42 + V43 + V44 + V45 + V46 + V47 + V48 + V49 + V50 + V51 + V52 + V53 + V54 + V55 + V56 + V57, data = trainData)
+  svmfit <- svm(V58 ~ V1 + V2 + V3 + V4 + V5 + V6 + V7 + V8 + V9 + V10 + V11 + V12 + V13 + V14 + V15 + V16 + V17 + V18 + V19 + V20 + V21 + V22 + V23 + V24 + V25 + V26 + V27 + V28 + V29 + V30 + V31 + V32 + V33 + V34 + V35 + V36 + V37 + V38 + V39 + V40 + V41 + V42 + V43 + V44 + V45 + V46 + V47 + V48 + V49 + V50 + V51 + V52 + V53 + V54 + V55 + V56 + V57, data = trainData)
   testData <- folds[[i]][,1:57] #test data is the current ith iteration.
-  predictedOutcomes[i] <- predict(svmfit[i], testData)
+  predictedOutcomes <- predict(svmfit, testData)
+  error[i] <- (sum((predictedOutcomes - folds[[i]][,58])^2)/nrow(folds[[i]]))^.5
 }
-
-traindata <- folds[-1]
-traindata <- do.call('rbind', traindata)
-svmfit1 <- svm(V58 ~ V1 + V2 + V3 + V4 + V5 + V6 + V7 + V8 + V9 + V10 + V11 + V12 + V13 + V14 + V15 + V16 + V17 + V18 + V19 + V20 + V21 + V22 + V23 + V24 + V25 + V26 + V27 + V28 + V29 + V30 + V31 + V32 + V33 + V34 + V35 + V36 + V37 + V38 + V39 + V40 + V41 + V42 + V43 + V44 + V45 + V46 + V47 + V48 + V49 + V50 + V51 + V52 + V53 + V54 + V55 + V56 + V57, data = traindata)
-print(svmfit1)
-testdata <- folds[[1]][,1:57]
-predictedOutcome <- predict(svmfit1, testdata)
 
 ################# running neural networks on dataset ####################
 
