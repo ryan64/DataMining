45e9d6318f384a9c76f1f818fd38aefb5b27d875
ryan.gonzalez64@gmail.com <ryan.gonzalez64@gmail.com>
2018-04-11 16:42:48 -0500

    finished svm and now finished nueral networks

--- Assignment3/Assignment3.R
+++ Assignment3/Assignment3.R
@@ -9,6 +9,14 @@
 #                          #
 ############################
 
+###########################################################
+
+install.packages("neuralnet")
+install.packages("e1071")
+
+library(neuralnet)
+library(e1071)
+
 file <- ("/home/ryan/Desktop/DataMining/Assignment3/spambase/spambase.data") #Romita, please change location of dataset 'spambase.data'
 spambase <- read.table(file, header = FALSE, sep = ",")
 
@@ -21,18 +29,38 @@ spamnorm$V58 = spambase[,58]
 folds = split(spamnorm, sample(1:10, nrow(spamnorm), replace=T))
 
 ################# running svm on dataset ##################
-install.packages("e1071")
-library(e1071)
 
-error = list()
+errorsForSVM = list()
 for (i in 1:10){
   trainData <- folds[-i] #trained data is every fold except for the current ith iteration.
   trainData = do.call('rbind', trainData)
   svmfit <- svm(V58 ~ V1 + V2 + V3 + V4 + V5 + V6 + V7 + V8 + V9 + V10 + V11 + V12 + V13 + V14 + V15 + V16 + V17 + V18 + V19 + V20 + V21 + V22 + V23 + V24 + V25 + V26 + V27 + V28 + V29 + V30 + V31 + V32 + V33 + V34 + V35 + V36 + V37 + V38 + V39 + V40 + V41 + V42 + V43 + V44 + V45 + V46 + V47 + V48 + V49 + V50 + V51 + V52 + V53 + V54 + V55 + V56 + V57, data = trainData)
   testData <- folds[[i]][,1:57] #test data is the current ith iteration.
   predictedOutcomes <- predict(svmfit, testData)
-  error[i] <- (sum((predictedOutcomes - folds[[i]][,58])^2)/nrow(folds[[i]]))^.5
+  errorsForSVM[i] <- (sum((predictedOutcomes - folds[[i]][,58])^2)/nrow(folds[[i]]))^.5
 }
+
+averageError = do.call('sum', errorsForSVM)/10
 
 ################# running neural networks on dataset ####################
 
+errorsForNN = list()
+
+for(i in 1:10){
+  #includes all but i-th fold
+  traindata = folds[-i]
+  
+  #makes the folds into one dataframe again
+  traindata = do.call('rbind', traindata)
+  
+  NN = neuralnet(V58 ~ V1 + V2 + V3 + V4 + V5 + V6 + V7 + V8 + V9 + V10 + V11 + V12 + V13 + V14 + V15 + V16 + V17 + V18 + V19 + V20 + V21 + V22 + V23 + V24 + V25 + V26 + V27 + V28 + V29 + V30 + V31 + V32 + V33 + V34 + V35 + V36 + V37 + V38 + V39 + V40 + V41 + V42 + V43 + V44 + V45 + V46 + V47 + V48 + V49 + V50 + V51 + V52 + V53 + V54 + V55 + V56 + V57, data = traindata, linear.output = TRUE, hidden = 2)
+  
+  #predicts using the fold not used in the training set
+  predictedData = compute(NN, folds[[i]][,c(1:57)])
+  
+  #calculates root mean square error
+  errorsForNN[i] = (sum((predictedData$net.result - folds[[i]][,58])^2) / nrow(folds[[i]])) ^ 0.5
+}
+
+#average error which is used as final error
+NNAvgError = do.call('sum',errorsForNN)/10
